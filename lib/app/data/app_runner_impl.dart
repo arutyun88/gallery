0b2f8e489e98f45c7e2fc8224747cad680b73fbb
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-01-12 22:14:31 +0400

    app: hydrated: save state

--- lib/app/data/app_runner_impl.dart
+++ lib/app/data/app_runner_impl.dart
@@ -2,6 +2,8 @@ import 'package:flutter/widgets.dart';
 import 'package:gallery/app/di/init_di.dart';
 import 'package:gallery/app/domain/app_builder.dart';
 import 'package:gallery/app/domain/app_runner.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
 class AppRunnerImpl implements AppRunner {
   @override
@@ -13,6 +15,9 @@ class AppRunnerImpl implements AppRunner {
   @override
   Future<void> run(AppBuilder appBuilder) async {
     await preloadData();
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await path_provider.getApplicationDocumentsDirectory(),
+    );
     runApp(appBuilder.buildApp());
   }
 }
