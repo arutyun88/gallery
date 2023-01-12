4df8fb9d8eb6accd1f0d209a322e008bfa439308
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-01-12 22:13:19 +0400

    app: start screen correcting

--- lib/app/data/app_builder_impl.dart
+++ lib/app/data/app_builder_impl.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gallery/app/domain/app_builder.dart';
 import 'package:gallery/app/ui/const/app_theme.dart';
+import 'package:gallery/feature/auth/ui/welcome_screen.dart';
 
 class AppBuilderImpl implements AppBuilder {
   @override
@@ -9,7 +10,7 @@ class AppBuilderImpl implements AppBuilder {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
       title: 'Gallery',
-      home: const Scaffold(),
+      home: const WelcomeScreen(),
     );
   }
 }
